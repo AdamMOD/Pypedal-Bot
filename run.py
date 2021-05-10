97cc8bdea8b7003e63c181c15d70dde79a063a87
Adam <adamthemodguy@gmail.com>
2021-05-10 14:27:59 -0700

    test continuous motion

--- run.py
+++ run.py
@@ -12,26 +12,11 @@ from board import SCL, SDA
 
 mybot = bot.Bot()
 
-maxcmd = np.array([60.0, 60., 60., 60.])
-for servo, cmd in enumerate(maxcmd):
-    mybot.command_servo_angle(servo, cmd)
-time.sleep(1)
-for i in range(4):
-    print(mybot.read_servo_angle(i))
-
-
-mincmd = maxcmd * -1
-for servo, cmd in enumerate(mincmd):
-    mybot.command_servo_angle(servo, cmd)
-time.sleep(1)
-for i in range(4):
-    print(mybot.read_servo_angle(i))
-
 mybot.zero_servos()
 time.sleep(1)
 for i in range(4):
     print(mybot.read_servo_angle(i))
-"""
+
 t_start = round(time.time() * 1000)
 currtime = round(time.time() * 1000)
 
@@ -41,11 +26,10 @@ dat_arr = []
 
 while(currtime - t_start < 10000):
     currtime = round(time.time() * 1000)
-    cmd = 60 + 20 * np.sin(float(currtime - t_start) * 2 / 1000)
+    cmd = 20 * np.sin(float(currtime - t_start) * 2 / 1000)
     mybot.command_servo_angle(1, cmd)
     reading = mybot.read_servo_angle(1)
     dat_arr.append([currtime - t_start, cmd, reading])
 
-
 print(dat_arr)
-"""
+
