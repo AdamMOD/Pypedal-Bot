32b8ffca9c12186c7c20a73b098c036f48549563
Adam <adamthemodguy@gmail.com>
2021-05-10 15:05:10 -0700

    data analysis

---
+++ data_analysis.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas
+import matplotlib.pyplot as plt
+
+with open("data\\servotsts.csv") as f:
+    servo_pd = pandas.read_csv(f)
+    f.close()
+
+print(servo_pd.head())
+plt.title("Y vs U for SG995 Left Hip")
+plt.scatter(servo_pd["Time"], servo_pd["Lefthip"], color = "tab:red", label="Y_Left")
+plt.scatter(servo_pd["Time"], servo_pd["Righthip"], color = "tab:green", label="Y_Right")
+plt.scatter(servo_pd["Time"], servo_pd["Command"], color = "tab:blue", label="U")
+plt.ylabel("Servo angle Y (degrees)")
+plt.xlabel("Time (ms)")
+
+plt.legend()
+plt.show()
