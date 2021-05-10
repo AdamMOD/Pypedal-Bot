268aea2404bb37520d05a988366f085c7c6a7729
Adam <adamthemodguy@gmail.com>
2021-05-10 14:09:34 -0700

    zero cal

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ class Bot:
             self.kit.servo[channel].acuation_range = 180
             self.kit.servo[channel].set_pulse_width_range(900, 2100)
 
-        self.servo_zero_angles = np.array([90.0, 65.0, 90.0, 70.0])
+        self.servo_zero_angles = np.array([115.0, 65.0, 110.0, 70.0])
         self.servo_angle_flips = np.array([1, -1, -1, 1])
 
         self.ads = adafruit_ads1x15.ads1115.ADS1115(self.__i2cbus)
