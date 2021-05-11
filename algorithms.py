3592fe5cdb06e04147fb318a59daf2ea86ac7d1d
Adam <adamthemodguy@gmail.com>
2021-05-10 21:36:20 -0700

    err

--- algorithms.py
+++ algorithms.py
@@ -6,4 +6,4 @@ def pd_control(state):
     """State is in form righthip angle, pitch, pitch rate"""
     err = np.array([0, zero_pitch, 0]) - state
     K = np.array([0, .1, 0])
-    return np.dot(err, K_p)
+    return np.dot(err, K)
