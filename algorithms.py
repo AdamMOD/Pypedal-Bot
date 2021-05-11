d10574b17fc6ae6e3716403b9f95ef087d29e5ea
Adam <adamthemodguy@gmail.com>
2021-05-10 22:21:58 -0700

    norate

--- algorithms.py
+++ algorithms.py
@@ -5,5 +5,5 @@ zero_pitch = 0.0
 def pd_control(state):
     """State is in form righthip angle, pitch, pitch rate"""
     err = np.array([0, zero_pitch, 0]) - state
-    K = np.array([.1, 1.5, 1])
+    K = np.array([.1, 1.5, 0])
     return np.dot(err, K)
