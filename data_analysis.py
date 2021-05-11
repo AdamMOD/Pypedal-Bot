4aa91e5ae8a0bab014f53648c0e1a882139f5ba9
Adam <adamthemodguy@gmail.com>
2021-05-10 21:42:46 -0700

    pid tune

--- data_analysis.py
+++ data_analysis.py
@@ -2,17 +2,17 @@ import numpy as np
 import pandas
 import matplotlib.pyplot as plt
 
-with open("data\\servotsts.csv") as f:
+with open("data\\runtst.csv") as f:
     servo_pd = pandas.read_csv(f)
     f.close()
 
 print(servo_pd.head())
 plt.title("Y vs U for SG995 Left Hip")
 #plt.scatter(servo_pd["Time"], servo_pd["Lefthip"], color = "tab:red", label="Y_Left")
-#plt.scatter(servo_pd["Time"], servo_pd["Righthip"], color = "tab:green", label="Y_Right")
+plt.scatter(servo_pd["Time"], servo_pd["Righthip"], color = "tab:green", label="Y_Right")
 plt.scatter(servo_pd["Time"], servo_pd["Pitch"], color = "tab:orange", label="Pitch")
 plt.scatter(servo_pd["Time"], servo_pd["Pitchdot"], color = "tab:purple", label="Pitch_rate")
-#plt.scatter(servo_pd["Time"], servo_pd["Command"], color = "tab:blue", label="U")
+plt.scatter(servo_pd["Time"], servo_pd["Command"], color = "tab:blue", label="U")
 plt.ylabel("Servo angle Y (degrees)")
 plt.xlabel("Time (ms)")
 
