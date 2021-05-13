73b3d28c7436a49a08568908dc14f3b5ae7b826c
Adam <adamthemodguy@gmail.com>
2021-05-13 11:18:47 -0700

    why so slow

--- data_analysis.py
+++ data_analysis.py
@@ -2,16 +2,16 @@ import numpy as np
 import pandas
 import matplotlib.pyplot as plt
 
-with open("data\\runtst.csv") as f:
+with open("data\\hip_sysiden\\servotsts.csv") as f:
     servo_pd = pandas.read_csv(f)
     f.close()
 
 print(servo_pd.head())
 plt.title("Y vs U for SG995 Left Hip")
-#plt.scatter(servo_pd["Time"], servo_pd["Lefthip"], color = "tab:red", label="Y_Left")
-plt.scatter(servo_pd["Time"], servo_pd["Righthip"], color = "tab:green", label="Y_Right")
-plt.scatter(servo_pd["Time"], servo_pd["Pitch"], color = "tab:orange", label="Pitch")
-plt.scatter(servo_pd["Time"], servo_pd["Pitchdot"], color = "tab:purple", label="Pitch_rate")
+plt.scatter(servo_pd["Time"], servo_pd["Lefthip"], color = "tab:red", label="Y_Left")
+#plt.scatter(servo_pd["Time"], servo_pd["Righthip"], color = "tab:green", label="Y_Right")
+#plt.scatter(servo_pd["Time"], servo_pd["Pitch"], color = "tab:orange", label="Pitch")
+#plt.scatter(servo_pd["Time"], servo_pd["Pitchdot"], color = "tab:purple", label="Pitch_rate")
 plt.scatter(servo_pd["Time"], servo_pd["Command"], color = "tab:blue", label="U")
 plt.ylabel("Servo angle Y (degrees)")
 plt.xlabel("Time (ms)")
