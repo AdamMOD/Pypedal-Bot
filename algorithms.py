ff80ad95f0e0fa6afeed50a5854eef3e46237105
Adam <adamthemodguy@gmail.com>
2021-05-13 23:03:20 -0700

    what

--- algorithms.py
+++ algorithms.py
@@ -15,8 +15,8 @@ def pid_control(state, dt):
     """State is in form righthip angle, pitch, pitch rate"""
     state = raw_read_to_incline(state)
     err = np.array([zero_pitch, 0]) - state
-    I_term = I_term + dt * err[1]
-    K = np.array([1, 0, 0])
+    I_term += dt * err[1]
+    K = np.array([0, 1])
     return np.dot(err, K) + I_gain * I_term
 
 
